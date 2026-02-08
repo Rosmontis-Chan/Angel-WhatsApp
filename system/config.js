38e1907784f4df3356a76923b6a10684aff57d55
エリシア ❄️ <152463971+Rosmontis-Chan@users.noreply.github.com>
2026-02-09 00:32:30 +0700

    Add files via upload

---
+++ system/config.js
@@ -0,0 +1,12 @@
+//========HELO FRIEND========//
+global.prefix = [".", "!", ".", ",", "🐤", "🗿"]; 
+global.publik = true
+global.owner = ["6282225336423"] 
+global.namabot = 'ler sanzy'
+//======================
+global.mess = { 
+owner: '*waduhh!, lu bukan owner gw bg*',
+premium: '*anda bukan user premium*',
+succes: '*done bang*'
+}
+//======================
